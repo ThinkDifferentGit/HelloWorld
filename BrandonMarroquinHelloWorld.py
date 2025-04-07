49adab328054fdc94b0e2da9383d3385ae9cb276
ThinkDifferentGit <brandonftwas@gmail.com>
2025-04-07 13:53:27 -0700

    Add files via upload

---
+++ BrandonMarroquinHelloWorld.py
@@ -0,0 +1,34 @@
+print('Hello world!')
+# ============================================================================
+# Source: Brandon Marroquin
+# https://github.com/ThinkDifferentGit
+# ThinkDifferentGit
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:        BrandonMarroquin('Hello world!')
+#Hello World!!!!!!
+# Dependencies:   python3
+#
+#
+# Revision:
+#  Revision 0.03 - Updated 06/09/2025 Added Hello World!
+#  Revision 0.02 - Updated 01/09/2025
+#  Revision 0.01 - Created 01/07/2025
+# Additional Comments: Testing
+#
+# ============================================================================
+
+def main():
+   # print('Hello world!')
+   # You can call other functions from here
+   x=2
+   y=6
+   z=x+y
+   print(z)
+   # All done
+
+# This is the call to the Function main().  You should always
+# have a main() and def main(): as part of all your programs
+main()
